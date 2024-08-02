bed1224ef7d91fcb67ba720cb57d9af805c3cff1
Hanrann6 <yookrann@ewhain.net>
2024-08-02 13:30:49 +0900

    [BE] feat: User 기능추가

---
+++ BE/src/main/java/com/example/BE/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.BE.repository;
+
+import com.example.BE.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
