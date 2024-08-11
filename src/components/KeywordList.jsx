e178b735b1c22ff156f81a7551d536496ec4afe5
minsol4171 <sol4171@naver.com>
2024-08-12 04:42:50 +0900

    [FE] feat: KeywordList 컴포넌트 생성

--- src/components/KeywordList.jsx
+++ src/components/KeywordList.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import data from "../data.json";
 
-const Keyword = styled.span`
+const Keyword = styled.div`
   margin-right: 10px;
   margin-top: 0;
   padding: 8px 12px;
@@ -12,23 +13,53 @@ const Keyword = styled.span`
   font-size: 12px;
   font-weight: 600;
   text-align: center;
-  display: flex;
+  display: inline-flex;
   align-items: center;
+  white-space: nowrap;
 `;
 
-const Wrapper = styled.div`
+const KeywordWrapper = styled.div`
   padding-left: 30px;
   display: flex;
   justify-content: left;
 `;
 
-function KeywordList() {
+const keywordText = {
+  //keywordMap은 자바스크립트 객체이다.
+  socketYN: "콘센트",
+  typingYN: "타이핑",
+  whispherYN: "소근소근 대화",
+  loudYN: "큰소리로 대화",
+  teamYN: "팀플",
+  lieYN: "누울 수 있는",
+  eatYN: "취식",
+  convYN: "편의시설",
+  cafeYN: "식음료점",
+  computerYN: "PC",
+  reserveYN: "공간 예약",
+  seatYN: "좌석 발급",
+};
+
+function KeywordList({ facId }) {
+  //json파일의 facility 중 facId가 props로 받은 값과 같은 객체를 뽑아 새로운 facility 배열에 저장
+  const facility = data.facility.find((fac) => fac.facId === facId);
+
+  //facility배열에서 value가 true인 'key'들을 추출해 새로운 배열 keywords에 저장
+  const keywords = Object.keys(facility).filter(
+    (key) => facility[key] === true
+  );
+
+  //keywords를 keywordText객체에서 찾아서 value를 새로운 배열 keywordList에 저장
+  const keywordList = keywords.map((key) => keywordText[key]);
+
   return (
-    <Wrapper>
-      <Keyword>콘센트</Keyword>
-      <Keyword>큰 소리로 대화</Keyword>
-      <Keyword>소근소근 대화</Keyword>
-    </Wrapper>
+    <div>
+      <KeywordWrapper>
+        {keywordList.map((key) => (
+          <Keyword>{key}</Keyword>
+        ))}
+      </KeywordWrapper>
+    </div>
   );
 }
 
