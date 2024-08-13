e38c700aa30bca79506aab232b678785914ea833
ruiiary <guidews39@gmail.com>
2024-08-13 14:27:42 +0900

    [FE] feat: NavBar 컴포넌트 추가

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,14 +1,17 @@
+import React, { Component } from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login.jsx';
-import SignUp from './pages/SignUp.jsx';
+import Home from "./pages/Home.jsx";
+import "./App.css";
+
+//import Login from './pages/Login.jsx';
+//import SignUp from './pages/SignUp.jsx';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/Login" element={<Login />} />
-          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
