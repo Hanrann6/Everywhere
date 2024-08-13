33707306d69c8bd40fff4939374db2e95a4302ee
ttf416 <thefirstfruit416@gmail.com>
2024-08-13 21:30:56 +0900

    [FE] Fix

--- FE/src/pages/Home.jsx
+++ FE/src/pages/Home.jsx
@@ -1,39 +1,19 @@
 import styled from 'styled-components';
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-
 import NavBar from '../components/NavBar';
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-function Home() {
-  /*
+/*
   const navigate = useNavigate();
   
   function goToLogin() {
   
   }
   */
-
+const Home = props => {
   return (
     <div className="root">
       <NavBar />
     </div>
   );
-}
-=======
-=======
->>>>>>> d1fccbd5110065af40a3caf7bd8f621ea9924300
-const Home = props => {
-  return (
-    <div className="root">
-      <NavBar></NavBar>
-    </div>
-  );
 };
-<<<<<<< HEAD
->>>>>>> d1fccbd5110065af40a3caf7bd8f621ea9924300
-=======
->>>>>>> d1fccbd5110065af40a3caf7bd8f621ea9924300
 
 export default Home;
