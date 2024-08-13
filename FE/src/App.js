79c6185638ea3e29e458e9c504027e54c5192b97
minsol4171 <sol4171@naver.com>
2024-08-13 17:48:57 +0900

    [FE] feat: 시설 상세 페이지 구현

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme.js';
+
 import Login from './pages/Login.jsx';
+import SignUp from './pages/Signup.jsx';
+import Home from './pages/Home.jsx';
 import Facility from './pages/Facility';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          
-          <Route path="fac/:facId" element={<Facility />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/Login" element={<Login />} />
+            <Route path="/SignUp" element={<SignUp />} />
+            <Route path="/" element={<Home />} />
+            <Route path="fac/:facId" element={<Facility />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
-
 export default App;
