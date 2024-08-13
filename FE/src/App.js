12914adc3bf25874cf5dccd54a6f5fb44ac2b1a9
minsol4171 <sol4171@naver.com>
2024-08-13 17:00:06 +0900

    에러사항 수정

--- FE/src/App.js
+++ FE/src/App.js
@@ -2,15 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login.jsx';
 import SignUp from './pages/SignUp.jsx';
-<<<<<<< HEAD
 import Facility from './pages/Facility';
-=======
-import Home from "./pages/Home.jsx";
-import "./App.css";
-
-//import Login from './pages/Login.jsx';
-//import SignUp from './pages/SignUp.jsx';
->>>>>>> c71a839d45bf44c9a5a2636bba3421ff80390eed
 
 function App() {
   return (
@@ -18,13 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/Login" element={<Login />} />
-<<<<<<< HEAD
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="fac/:facId" element={<Facility />} />
-=======
-          <Route path="/SignUp" element={<SignUp />} /> 
-          <Route path="/" element={<Home />} />
->>>>>>> c71a839d45bf44c9a5a2636bba3421ff80390eed
         </Routes>
       </BrowserRouter>
     </div>
