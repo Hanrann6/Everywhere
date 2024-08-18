d21b803d0f5818b7ddcf3cae6a178dd2f9402f94
minsol4171 <sol4171@naver.com>
2024-08-18 23:20:45 +0900

    [FE] feat: 검색 결과 컴포넌트 구현

---
+++ FE/src/components/Result/Result.jsx
@@ -0,0 +1,26 @@
+import * as S from './Result.styles';
+import { ResultBox } from './ResultBox';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnglesDown } from '@fortawesome/free-solid-svg-icons';
+
+export function Result({ results }) {
+  return (
+    <S.Wrapper>
+      <S.ResultNumber>
+        <FontAwesomeIcon
+          icon={faAnglesDown}
+          size="2xl"
+          style={{ color: '#00462A' }}
+        />
+        <S.ShowText>총 {results.length}건의 검색 결과</S.ShowText>
+      </S.ResultNumber>
+
+      <S.ResultList>
+        {results.map(facility => (
+          <ResultBox key={facility.id} facility={facility} />
+        ))}
+      </S.ResultList>
+    </S.Wrapper>
+  );
+}
