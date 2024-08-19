6f51f3b3619515a9412e203513edc87963ed3ca9
Hanrann6 <yookrann@ewhain.net>
2024-08-19 16:06:49 +0900

    [BE] feat: RS 설치

--- BE/src/main/java/com/example/BE/BeApplication.java
+++ BE/src/main/java/com/example/BE/BeApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeApplication {
+	static {
+		System.setProperty("com.amazons.sdk.disableEc2Metadata", "true");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeApplication.class, args);
---
+++ BE/src/main/java/com/example/BE/config/S3Config.java
@@ -0,0 +1,27 @@
+package com.example.BE.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+    @Value("${cloud.aws.credentials.access-key}")
+    private String iamAccessKey;
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String iamSecretKey;
+    @Value("${cloud.aws.region.static}")
+    private String region;
+    @Bean
+    public AmazonS3Client amazonS3Client(){
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(iamAccessKey, iamSecretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region).enablePathStyleAccess()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
