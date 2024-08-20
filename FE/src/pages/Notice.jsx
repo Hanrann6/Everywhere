26782df659c7889574d915d712c5a59261df6a2d
minsol4171 <sol4171@naver.com>
2024-08-21 06:21:27 +0900

    [FE] feat: 공지사항 페이지 구현

---
+++ FE/src/pages/Notice.jsx
@@ -0,0 +1,57 @@
+import * as S from '../styles/notice';
+import { NavBar } from '../components';
+import { GoBack } from '../components/common/GoBack';
+import { Footer } from '../components/common/Footer';
+import { FiVolume2 } from 'react-icons/fi';
+
+function Notice() {
+  return (
+    <S.Wrapper>
+      <NavBar />
+
+      <S.Header>
+        <S.Button>
+          <GoBack />
+        </S.Button>
+
+        <S.Title>공지사항</S.Title>
+      </S.Header>
+
+      <S.List>
+        <S.Notice>
+          <FiVolume2 size={20} style={{ marginRight: '10px' }}></FiVolume2>
+          <S.Text>이용약관</S.Text>
+        </S.Notice>
+        <S.Notice>
+          <FiVolume2 size={20} style={{ marginRight: '10px' }}></FiVolume2>
+          <S.Text>개인정보처리방침</S.Text>
+        </S.Notice>
+        <S.Notice>
+          <FiVolume2 size={20} style={{ marginRight: '10px' }}></FiVolume2>
+          <S.Text
+            onClick={() =>
+              window.open('https://www.ewha.ac.kr/ewha/life/place.do')
+            }
+          >
+            공간 사용 안내
+          </S.Text>
+        </S.Notice>
+        <S.Notice>
+          <S.Text
+            onClick={() =>
+              window.open('https://lib.ewha.ac.kr/bbs/content/1_37285')
+            }
+          >
+            [중앙도서관] 도서관 1층 라운지, 노트북실 이용중지 (8/1~9/14)
+          </S.Text>
+        </S.Notice>
+      </S.List>
+
+      <S.FooterWrapper>
+        <Footer />
+      </S.FooterWrapper>
+    </S.Wrapper>
+  );
+}
+
+export default Notice;
