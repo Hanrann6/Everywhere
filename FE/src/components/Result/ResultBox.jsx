ec1b745ac6ea5f87c12144da3fdcc7ed2f409c7c
minsol4171 <sol4171@naver.com>
2024-08-21 00:29:24 +0900

    [FE] fix: Result컴포넌트 수정 및 변경된 파일 삭제

--- FE/src/components/Result/ResultBox.jsx
+++ FE/src/components/Result/ResultBox.jsx
@@ -3,6 +3,10 @@ import { useNavigate } from 'react-router-dom';
 
 import { FacilityImage } from '../Details';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+
 export function ResultBox({ facility }) {
   const navigate = useNavigate();
   const onClick = () => {
@@ -13,15 +17,27 @@ export function ResultBox({ facility }) {
     <S.Box onClick={onClick}>
       <S.Image>
         <FacilityImage
-          width="275px"
-          height="180px"
-          imagePath={facility.imagePath}
+          width="285px"
+          height="190px"
+          imagePath={facility.post.imagePath}
         />
       </S.Image>
-      <S.PostName>{facility.postName}</S.PostName>
-      <S.Content>{facility.content}</S.Content>
-      <S.Location>{facility.location}</S.Location>
-      <S.Time>{facility.time}</S.Time>
+      <S.PostName>{facility.post.postName}</S.PostName>
+      <S.Content>{facility.post.content}</S.Content>
+      <S.GreenText>
+        <S.Location>
+          <FontAwesomeIcon
+            icon={faLocationDot}
+            style={{ marginRight: '6px' }}
+          />
+
+          {facility.post.location}
+        </S.Location>
+        <S.Time>
+          <FontAwesomeIcon icon={faClock} style={{ marginRight: '6px' }} />
+          {facility.post.time}
+        </S.Time>
+      </S.GreenText>
     </S.Box>
   );
 }
